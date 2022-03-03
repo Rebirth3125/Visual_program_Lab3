c50815e811dc30efa65e61db5ef9b6bec317485f
Rebirth3125 <79037935+Rebirth3125@users.noreply.github.com>
2022-03-03 22:11:40 +0700

    Create RomanNumberException

---
+++ Lab3/RomanNumberException.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace LR3
+{
+    public class RomanNumberException : Exception
+    {
+        public RomanNumberException() : base() { }
+        public RomanNumberException(string message) : base(message) { }
+    }
+}
